6dc12a402d6632d11fc57ace1fac1bfb71a95f77
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-15 21:48:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   create_an_array = ["air", "earth", "fire", "water"]
-  create_an_array << "light"
+  create_an_array << "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
